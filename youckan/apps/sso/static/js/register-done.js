cb4c224d80dfc19ec01cadb39f98eaddb720d00a
Axel Haustant <noirbizarre@gmail.com>
2013-12-20 12:26:49 +0100

    Upgraded to assets 0.2.54

--- youckan/apps/sso/static/js/register-done.js
+++ youckan/apps/sso/static/js/register-done.js
@@ -12,8 +12,7 @@
             .typeahead(Config.typeahead.organizations)
             .on('typeahead:selected typeahead:autocompleted', function(e, data, dataset) {
                 window.location = Config.urls.organization(data.name);
-            })
-            .siblings('input.tt-hint').addClass('hint-large');;
+            });
     });
 
 }(window.jQuery, window.EtalabConfig));
